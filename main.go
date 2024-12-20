62c5124834d1e13c7307f6ea9b5311d56dd104e5
jim <jimmyx27@gmail.com>
2024-12-20 11:44:34 -0500

    chirpy

--- main.go
+++ main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = ":8080"
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
+	fileserver := http.FileServer(http.Dir(filepathRoot))
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fileserver))
+	mux.Handle("/app/", fsHandler)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", validate_chirp)
+
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
-	log.Printf("server started")
+	log.Printf("serving files on port %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func health_check(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
 }
