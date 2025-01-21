fd490a4c6d79aa98b3615193faf97008f4207de1
jim <jimmyx27@gmail.com>
2025-01-20 19:17:15 -0500

    chirpy

--- reset.go
+++ reset.go
@@ -3,7 +3,13 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset only allowed in dev mode"))
+		return
+	}
 	cfg.fileserverHits.Store(0)
+	cfg.db.Reset(r.Context())
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0 and database reset"))
 }
